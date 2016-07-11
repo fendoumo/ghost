7c7ea1acd6953ac44ac05bffaa444087f7784398
fendou_mo <fendou_mo@163.com>
2016-07-11 13:32:20 +0800

    change py

--- ghosttest.py
+++ ghosttest.py
@@ -12,12 +12,6 @@ while True:
         pass
     #print sessin.content
     try:
-        page , resource = sessin.open("http://abcabc.gq")
-        sessin.wait_for_page_loaded(10000)
-    except:
-        pass
-    #print sessin.content
-    try:
         page , resource = sessin.open("http://abcabc.gq/test.php")
         sessin.wait_for_page_loaded(10000)
     except:
@@ -31,18 +25,6 @@ while True:
     #print sessin.content
     try:
         page , resource = sessin.open("http://mxqabc.gq/test.php")
-        sessin.wait_for_page_loaded(10000)
-    except:
-        pass
-    #print sessin.content
-    try:
-        page , resource = sessin.open("http://window.gq")
-        sessin.wait_for_page_loaded(10000)
-    except:
-        pass
-    #print sessin.content
-    try:
-        page , resource = sessin.open("http://window.gq/test.php")
         sessin.wait_for_page_loaded(10000)
     except:
         pass
